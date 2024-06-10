b692fbe6a294e4c52a47b8e74c60adb4132d82fa
Jerby <mrmyst0789@gmail.com>
2024-06-10 16:24:20 +0800

    Added Post-Processing
    + Post processing for blackout testing via color grading/ filter
    
    + Sky material for post processing usage

--- Assets/Scripts/Hazards/BlackoutEffect.cs
+++ Assets/Scripts/Hazards/BlackoutEffect.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Rendering.PostProcessing;
+
 
 public class BlackoutEffect : MonoBehaviour
 {
-    // Start is called before the first frame update
-         void Start()
-         {
-             
-         }
-     
-         // Update is called once per frame
-         void Update()
-         {
-             
-         }
+    
+    [SerializeField] private PostProcessVolume _postProcessVolume;
+    private ColorGrading _colorGrading;
+    [SerializeField] private float _hazardDuration = 5f;
+    void Start()
+    {
+        _postProcessVolume = GetComponent<PostProcessVolume>();
+        _postProcessVolume.profile.TryGetSettings(out _colorGrading);
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown("k"))
+        {
+            StartCoroutine("blackout");
+        }
+    }
+    
+    IEnumerator blackout()
+    {
+        Debug.Log("Screen goes bye :<");
+        _colorGrading.colorFilter.value = Color.black;
+        yield return new WaitForSeconds(_hazardDuration);
+        Debug.Log("Screen goes hi!");
+        _colorGrading.colorFilter.value = Color.white;  
+    }
 }
