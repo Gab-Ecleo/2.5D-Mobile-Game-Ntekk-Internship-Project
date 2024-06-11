3408c5fef8646d0b67e54ae0728e0a4abed7e6a8
Jerby <mrmyst0789@gmail.com>
2024-06-11 16:23:31 +0800

    Changed BlackoutEffects.cs
    + Changed blackout effects to basic "set active"
    - Not final, to be changed soon

--- Assets/Scripts/Hazards/BlackoutEffect.cs
+++ Assets/Scripts/Hazards/BlackoutEffect.cs
@@ -9,15 +9,9 @@ using UnityEngine.Rendering.Universal;
 
 public class BlackoutEffect : MonoBehaviour
 {
-    
-    [SerializeField] private Volume _postProcessVolume;
-    private ColorAdjustments _colorAdjustments;
+
+    [SerializeField] private GameObject _blackOut;
     [SerializeField] private float _hazardDuration = 5f;
-    void Start()
-    {
-        _postProcessVolume = GetComponent<Volume>();
-        _postProcessVolume.profile.TryGet(out _colorAdjustments);
-    }
     void Update()
     {
         if (Input.GetKeyDown("k"))
@@ -28,11 +22,10 @@ public class BlackoutEffect : MonoBehaviour
     
     IEnumerator blackout()
     {
+        _blackOut.SetActive(true);
         Debug.Log("Screen goes bye :<");
-        //_colorAdjustments.colorFilter.
-        _colorAdjustments.colorFilter.value = Color.black;
         yield return new WaitForSeconds(_hazardDuration);
         Debug.Log("Screen goes hi!");
-        _colorAdjustments.colorFilter.value = Color.white;  
+        _blackOut.SetActive(false);
     }
 }
