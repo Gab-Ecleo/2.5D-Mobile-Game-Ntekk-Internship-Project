f7a1d6ecc043a328497f7dfc395a6a7f0e7c6487
Jerby <mrmyst0789@gmail.com>
2024-06-18 16:07:50 +0800

    Updated PressureEffect script
    +Added random side push
    - Loop not working for now to make it run (still wont push the character consistently)

--- Assets/Scripts/Hazards/PressureEffect.cs
+++ Assets/Scripts/Hazards/PressureEffect.cs
@@ -6,36 +6,46 @@ using PlayerScripts;
 using ScriptableData;
 using Unity.Mathematics;
 using UnityEngine;
+using Random = UnityEngine.Random;
 using Vector2 = UnityEngine.Vector2;
 using Vector3 = UnityEngine.Vector3;
 
 public class PressureEffect : MonoBehaviour
 {
     [SerializeField] private float _hazardDuration = 5f;
-    [SerializeField] private PlayerStatsSO currentPlayerStats;
 
     [SerializeField] private Rigidbody _playerRB;
-    [SerializeField] private GameObject _player;
     
     [SerializeField] private float _windStr;
     [SerializeField] private Vector3 _windDir;
+    public Vector3 randomVal;
 
-   
+
     private void FixedUpdate()
     {
-       
+        randomVal = new Vector3(Random.Range(1, 3), 0, 0);
+        if (randomVal.x == 1)
+        {
+            _windDir.x = -5;
+        }
+        else if (randomVal.x == 2)
+        {
+            _windDir.x = 5;
+        }
         // CHANGE TRIGGER in the Future!!! Activates Hazard when button is pressed
+        
         if (Input.GetKeyDown("j"))
         {
-           StartCoroutine("windOn");
+            StartCoroutine("windOn");
         }
     }
 
+    // NOT LOOPING YET !! WIP :<
     void windBlow(){
-        for (int i = 0; i <= _hazardDuration; i++)
+        for (int i = 0; i <= 5; i++)
         {
-            Debug.Log("Pushing player :<");
             _playerRB.AddForce (_windDir * _windStr);
+            Debug.Log("Pushing player :<");
         }
         
     }
@@ -43,11 +53,9 @@ public class PressureEffect : MonoBehaviour
     //Pushes the player for [Hazard Duration], then returns to the original value
     private IEnumerator windOn()
     {
-        windBlow();
+        //windBlow();
+        _playerRB.AddForce (_windDir * _windStr);
         Debug.Log("Push push push :>");
         yield return new WaitForSeconds(_hazardDuration);
-        
-        Debug.Log("Returning :>");
-        
     }
 }
