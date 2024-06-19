b83b6f22f4f1dd4466afb9d929047ad5d8b1553c
Jerby <mrmyst0789@gmail.com>
2024-06-19 12:06:14 +0800

    Updated scripts and scene
    + Added DefaultMaterial for physic material changing
    + Updated script to change materials on demand
    - Removed slope as intended

--- Assets/Scripts/Hazards/IceEffect.cs
+++ Assets/Scripts/Hazards/IceEffect.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class IceEffect : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private PhysicMaterial _slipperyMaterial;
+    [SerializeField] private PhysicMaterial _defaultMaterial;
+    [SerializeField] private Collider _platformColl;
+    [SerializeField] private Collider _blockColl;
+    
+    [SerializeField] private float _hazardDuration = 5f;
+    
+    
+    private void Update()
     {
-        
+        // CHANGE TRIGGER in the Future!!! Activates Hazard when button is pressed
+        if (Input.GetKeyDown("u"))
+        {
+            StartCoroutine("slideOn");
+        }
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    //Changes the material of the objects to a slippery ice material for [Hazard Duration], then returns to the original material
+    IEnumerator slideOn()
     {
-        
+        _platformColl.material = _slipperyMaterial;
+        _blockColl.material = _slipperyMaterial;
+        Debug.Log("Slide them platforms :<");
+        yield return new WaitForSeconds(_hazardDuration);
+        _platformColl.material = _defaultMaterial;
+        _blockColl.material = _defaultMaterial;
+        Debug.Log("Returning Platform material :>");
     }
 }
