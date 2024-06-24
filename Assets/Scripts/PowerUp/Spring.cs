4cbb68c3ec9d4223827defddde984bbc461179d0
KyleReyes43 <67773976+KyleReyes43@users.noreply.github.com>
2024-06-24 13:04:12 +0800

    Updated Spring Script
    -Spring Power up Script function added
    -Turned Power up Cube to a Prefab specifically for Spring power up

--- Assets/Scripts/PowerUp/Spring.cs
+++ Assets/Scripts/PowerUp/Spring.cs
@@ -1,18 +1,40 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using ScriptableData;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Spring : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float _powerDuration = 3f;
+    [SerializeField] private PlayerStatsSO currentPlayerStats;
+    [SerializeField] private float _currentJumpHeight;
+    [SerializeField] private float _jumpBoost = 3;
+    
+    
+    private void Start()
     {
-        
+        _currentJumpHeight = currentPlayerStats.jumpHeight;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
+        StartCoroutine("SpringPower");
         
+        //Destroy(gameObject);
     }
+
+    IEnumerator SpringPower()
+    {
+        _currentJumpHeight += _jumpBoost;
+        currentPlayerStats.jumpHeight = _currentJumpHeight;
+        Debug.Log("Spring On");
+        yield return new WaitForSeconds(_powerDuration);
+        _currentJumpHeight -= _jumpBoost;
+        currentPlayerStats.jumpHeight = _currentJumpHeight;
+        Debug.Log("Spring Off");
+    }
+
+
 }
