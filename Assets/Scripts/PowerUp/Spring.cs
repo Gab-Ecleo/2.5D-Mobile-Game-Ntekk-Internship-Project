9191edde55c5586cd76f4cfffa16c51c7e954a1e
Gandrel-ikuzi <ecleo552@gmail.com>
2024-06-27 09:17:09 +0800

    [Minor Update] Script Changes
    - Remove and merged Spring duplicate

--- Assets/Scripts/PowerUp/Spring.cs
+++ Assets/Scripts/PowerUp/Spring.cs
@@ -5,8 +5,11 @@ using ScriptableData;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class Spring : MonoBehaviour
+public class Spring : PlayerPowerUps
 {
+    [Header("Spring Power-Up")]
+    [SerializeField] protected float SpringJumpHeight;
+    
     private PowerUpManager _powerUp;
 
     void Start()
@@ -20,6 +23,18 @@ public class Spring : MonoBehaviour
         StartCoroutine(_powerUp.SpringPower());
         GetComponent<Collider>().enabled = false;
         GetComponent<MeshRenderer>().enabled = false;
+    }
+    
+    protected override void OnPowerUpActive()
+    {
+        base.OnPowerUpActive();   
+        _currPlayerStatsSO.jumpHeight = SpringJumpHeight;
+    }
+
+    protected override void OnPowerUpDeactivate()
+    {
+        base.OnPowerUpDeactivate();
+        _currPlayerStatsSO.jumpHeight = _initialPlayerStatsSO.jumpHeight;
     }
 
 }
