459ba3e0a574e294bf9e79e6c9970c0056a90124
CentAlfaro <vincent.alfaro28@gmail.com>
2024-07-04 13:55:49 +0800

    Changed the implementation of the Block-Grid Movement
    - Changed the Block's movement checkers from Top and Bottom Raycast to checking the top and bottom references of the Grid Cell.

--- Assets/Scripts/BlockSystemScripts/BlockScripts/BlockScript.cs
+++ Assets/Scripts/BlockSystemScripts/BlockScripts/BlockScript.cs
@@ -13,19 +13,8 @@ namespace BlockSystemScripts.BlockScripts
         [Header("Block Fall Timer")]
         [SerializeField] private BlockFallTimer fallTimer;
 
-        [Header("Bottom Raycast References")] 
-        [SerializeField] private Vector3 directionBot = -Vector3.up;
-        [SerializeField] private float maxDistanceBot = 0.6f;
-        [SerializeField] private LayerMask landedLayerBot;
-        
-        [Header("Top Raycast References")] 
-        [SerializeField] private Vector3 directionTop = Vector3.up;
-        [SerializeField] private float maxDistanceTop = 0.6f;
-        [SerializeField] private LayerMask landedLayerTop;
-        private RaycastHit _hit1;
-        
         [Header("Test References. To be private")]
-        [SerializeField] private GridCell currentCell, nextCell;
+        [SerializeField] private GridCell currentCell;
         [SerializeField] private BlockSpawner blockSpawner;
 
         private bool _canPickUp;
@@ -35,13 +24,6 @@ namespace BlockSystemScripts.BlockScripts
         public GridCell CurrentCell => currentCell;
 
         #endregion
-        
-        //draws a ray for the raycast. CAN DELETE AFTER TESTING
-        private void FixedUpdate()
-        {
-            Debug.DrawRay(transform.position, directionBot * maxDistanceBot, Color.red);
-            Debug.DrawRay(transform.position, directionTop * maxDistanceTop, Color.green);
-        }
 
         #region BLOCK_MANIPULATION
         //called by the block spawner or the player to initialize references for this block 
@@ -50,7 +32,6 @@ namespace BlockSystemScripts.BlockScripts
             currentCell = cell;
             currentCell.FillCellSlot(this);
             fallTimer.StartTimer();
-            nextCell = currentCell.AssignedColumn.GridCells[currentCell.RowIndex + 1].GetComponent<GridCell>();
             blockSpawner = spawnerReference;
 
             TogglePickUpState(false);
@@ -83,24 +64,15 @@ namespace BlockSystemScripts.BlockScripts
             SignalTopBlock();
             
             //checks if nextCell has a value
-            if (nextCell != null)
+            if (currentCell.NextCell != null)
             {
                 currentCell.EmptyCellSlot();
-                currentCell = nextCell;
+                currentCell = currentCell.NextCell;
                 currentCell.FillCellSlot(this);
                 transform.position = currentCell.transform.position;
                 fallTimer.StartTimer();
-
                 TogglePickUpState(false);
             }
-            
-            //checks if the current cell is not yet the last one. If not, give nextCell a value. If it is, return null. 
-            if (currentCell.RowIndex + 1 != currentCell.AssignedColumn.GridCells.Count)
-            {
-                nextCell = currentCell.AssignedColumn.GridCells[currentCell.RowIndex + 1].GetComponent<GridCell>();
-                return;
-            }
-            nextCell = null;
         }
         
         private void TogglePickUpState(bool nextState)
@@ -119,21 +91,32 @@ namespace BlockSystemScripts.BlockScripts
         #endregion
         
         #region RAYCAST_METHODS
-        //casts a raycast, determining if there is a block or a platform below
+        //Determines if there is a block or a platform below
         public bool IsLanded()
         {
-            return Physics.Raycast(transform.position, directionBot, maxDistanceBot, landedLayerBot);
+            if (currentCell.NextCell == null)
+            {
+                return true;
+            }
+            if (currentCell.NextCell.CurrentBlock != null)
+            {
+                return true;
+            }
+            return false;
         }
         
-        //casts a raycast, determining if there is a block above
+        //Determines if there is a block above
         public BlockScript TopBlockDetection()
         {
-            Physics.Raycast(transform.position, directionTop, out _hit1,maxDistanceTop,  landedLayerTop);
-            if (_hit1.collider == null)
+            if (currentCell.PreviousCell == null)
             {
                 return null;
             }
-            return _hit1.collider.gameObject.GetComponent<BlockScript>();
+            if (currentCell.PreviousCell.CurrentBlock == null)
+            {
+                return null;
+            }
+            return currentCell.PreviousCell.CurrentBlock;
         }
 
         #endregion
