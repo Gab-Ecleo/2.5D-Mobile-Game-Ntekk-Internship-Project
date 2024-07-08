31b95b19683f6d9641a2d0c74d434cfe5b189098
Jerby <mrmyst0789@gmail.com>
2024-07-08 11:43:45 +0800

    Initial Commit
    + Added base script for Score to currency
    
    + added currency on player stats

---
+++ Assets/Scripts/ScoreSystem/ScoreToCurrency.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using ScriptableData;
+using UnityEngine;
+
+public class ScoreToCurrency : MonoBehaviour
+{
+    [SerializeField] private PlayerStatsSO initialPlayerStats;
+    [SerializeField] private PlayerStatsSO currentPlayerStats;
+    [SerializeField] private PlayerStatsSO coins;
+
+    [SerializeField] private ScoresSO Points;
+    
+    [SerializeField] private float score;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
