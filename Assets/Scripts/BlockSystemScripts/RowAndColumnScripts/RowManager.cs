06b083b6763c8682b768e60c7d987c8813793caf
CentAlfaro <vincent.alfaro28@gmail.com>
2024-08-07 15:27:21 +0800

    Updated Row Validation implementation
    - Added an enum containing two validation implementation types: Homogeneous Only and Heterogeneous clear
    - Added a dropdown list that determines which type of validation implementation would the rows use
    - Modified the row clear power up bottom ray detection

--- Assets/Scripts/BlockSystemScripts/RowAndColumnScripts/RowManager.cs
+++ Assets/Scripts/BlockSystemScripts/RowAndColumnScripts/RowManager.cs
@@ -10,6 +10,9 @@ namespace BlockSystemScripts.RowAndColumnScripts
     /// </summary>
     public class RowManager : AlignmentManager
     {
+        [SerializeField] private RowState rowState;
+        
+        [Header("Data References")]
         [SerializeField] private ScoresSO _playerScore;
         [SerializeField] private PlayerStatsSO _playerCurrStats;
      
@@ -29,26 +32,30 @@ namespace BlockSystemScripts.RowAndColumnScripts
                 //Then checks if the current block in that cell is Landed. If it is, add +1 ot the block counter
                 if (cell.CurrentBlock.BlockState is not (BlockState.Landed or BlockState.CanPickUp)) continue;
                 blockCounter++;
-                
-                //A validation run for checking if the blocks in the row are homogenous
-                sameColorCount++;
-                if (sameColorCount<=1)
+
+                //checks if the validation implemented is homogeneous only clear
+                if (rowState == RowState.HomoOnlyClear)
                 {
-                    //if this is the first block, set the color of this block as reference for the homogenous color clear
-                    colorType = cell.CurrentBlock.BlockType;
-                }
-                else
-                {
-                    //if the color type of the current block is different from the referenced color, reduce the same color counter
-                    if (cell.CurrentBlock.BlockType != colorType)
+                    //A validation run for checking if the blocks in the row are homogenous
+                    sameColorCount++;
+                    if (sameColorCount<=1)
                     {
-                        sameColorCount--;
+                        //if this is the first block, set the color of this block as reference for the homogenous color clear
+                        colorType = cell.CurrentBlock.BlockType;
+                    }
+                    else
+                    {
+                        //if the color type of the current block is different from the referenced color, reduce the same color counter
+                        if (cell.CurrentBlock.BlockType != colorType)
+                        {
+                            sameColorCount--;
+                        }
                     }
                 }
             }
             //If the number in the block counter is the same as the total number of cells in this row, clear the row
             if (blockCounter < GridCells.Count) return;
-            if (sameColorCount < GridCells.Count) return;
+            if (sameColorCount < GridCells.Count && rowState == RowState.HomoOnlyClear) return;
             ClearRow();
         }
 
@@ -56,11 +63,32 @@ namespace BlockSystemScripts.RowAndColumnScripts
         [ContextMenu("CLEAR ROW!")]
         public void ClearRow()
         {
+            var sameColorCount = 0;
+            var colorType = BlockType.None;
             foreach (var cell in GridCells)
             {
                 if (cell.CurrentBlock == null) continue;
                 if (cell.CurrentBlock.BlockType == BlockType.PowerUp) continue;
 
+                //checks if the validation implemented is not homogenous only clear
+                if (rowState == RowState.HeteroClear)
+                {
+                    //A validation run for checking if the blocks in the row are homogenous
+                    sameColorCount++;
+                    if (sameColorCount<=1)
+                    {
+                        //if this is the first block, set the color of this block as reference for the homogenous color clear
+                        colorType = cell.CurrentBlock.BlockType;
+                    }
+                    else
+                    {
+                        //if the color type of the current block is different from the referenced color, reduce the same color counter
+                        if (cell.CurrentBlock.BlockType != colorType)
+                        {
+                            sameColorCount--;
+                        }
+                    }
+                }
                 cell.DestroyBlock();
 
                 // add points and update ui when cleared
@@ -68,11 +96,11 @@ namespace BlockSystemScripts.RowAndColumnScripts
             }
             
             //Behavior for a homogenous color clear
-            // if (sameColorCount == GridCells.Count)
-            // {
-            //     ColorScore(sameColorCount);
-            //     Debug.Log("SAME COLORED ROW COMPLETED");
-            // }
+             if (sameColorCount == GridCells.Count && rowState == RowState.HeteroClear)
+             {
+                 ColorScore(sameColorCount);
+                 Debug.Log("SAME COLORED ROW COMPLETED");
+             }
         }
 
         [ContextMenu("Test clear ")]
@@ -96,6 +124,5 @@ namespace BlockSystemScripts.RowAndColumnScripts
             GameEvents.ON_SCORE_CHANGES?.Invoke(pointsToAdd, multiplier, hasMultiplier);
             GameEvents.ON_UI_CHANGES?.Invoke();
         }
-
     }
 }
