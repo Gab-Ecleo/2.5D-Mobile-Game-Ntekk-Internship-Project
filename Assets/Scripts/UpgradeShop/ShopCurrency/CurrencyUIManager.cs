a0ad87b35beca1c4949b64fee644d8f870a7d588
CentAlfaro <vincent.alfaro28@gmail.com>
2024-09-18 14:29:05 +0800

    Modified the Currency Manager References

--- Assets/Scripts/UpgradeShop/ShopCurrency/CurrencyUIManager.cs
+++ Assets/Scripts/UpgradeShop/ShopCurrency/CurrencyUIManager.cs
@@ -1,4 +1,5 @@
-﻿using TMPro;
+﻿using JetBrains.Annotations;
+using TMPro;
 using UnityEngine;
 
 namespace UpgradeShop.ShopCurrency
@@ -7,10 +8,22 @@ namespace UpgradeShop.ShopCurrency
     public class CurrencyUIManager : MonoBehaviour
     {
         [SerializeField] private TextMeshProUGUI currencyTxt;
-
+        [SerializeField] private TextMeshProUGUI matchCurrencyTxt;
+        
+        //called for the shop currency
         public void UpdateCurrencyUI(int updatedValue)
         {
+            // no value = no UI to update
+            if (currencyTxt == null) return;
             currencyTxt.text = updatedValue.ToString("D4");
+        }
+
+        //called for the in-match currency
+        public void UpdateMatchCurrencyUI(int updatedValue)
+        {
+            // no value = no UI to update
+            if (matchCurrencyTxt == null) return;
+            matchCurrencyTxt.text = updatedValue.ToString("D4");
         }
     }
 }
