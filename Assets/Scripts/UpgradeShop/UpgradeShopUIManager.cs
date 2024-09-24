fe7399a14ce8f29c22a66aadd51a97b7f1e6e6c7
CentAlfaro <vincent.alfaro28@gmail.com>
2024-09-24 10:25:56 +0800

    Added a UI checker that toggles the Confirm Button
    - Added an integer list that validates each upgrade shop item level, if they have any changes.
    - Increasing or Decreasing a respective upgrade item's level will update the respective integer value in the integer list.
    - Any changes in these levels will toggle the Confirm button into interactable

--- Assets/Scripts/UpgradeShop/UpgradeShopUIManager.cs
+++ Assets/Scripts/UpgradeShop/UpgradeShopUIManager.cs
@@ -1,7 +1,9 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
+using System.Collections.Generic;
 using EventScripts;
+using ScriptableData;
 using UnityEngine;
+using Button = UnityEngine.UI.Button;
 
 namespace UpgradeShop
 {
@@ -11,11 +13,19 @@ namespace UpgradeShop
         [SerializeField] private GameObject noMoneyUI;
         [SerializeField] private float uiTimer = 1f;
 
+        [Header("Confirm Button")] 
+        [SerializeField] private List<int> itemLevelCounter;
+        [SerializeField] private Button confirmButton;
+        private int _listPosition = 0;
+        private bool _noChanges = true;
+
         private bool _isCoroutineActive;
+        public bool NoChanges => _noChanges;
 
         #region INSUFFICIENT_FUNDS
         private void TriggerNoMoneyUI()
         {
+            if (noMoneyUI == null) return;
             StartCoroutine(NoMoneyUITimer());
         }
 
@@ -31,15 +41,73 @@ namespace UpgradeShop
             _isCoroutineActive = false;
         }
         #endregion
+
+        #region CONFIRM_BUTTON_HANDLER
+        public void InitializeIntList(UpgradeItemsList upgradeablesList)
+        {
+            foreach (var upgradeable in upgradeablesList.items)
+            {
+                itemLevelCounter.Add(0);
+            }
+        }
+
+        public void SyncPagePosition(int currentPage)
+        {
+            _listPosition = currentPage;
+        }
+
+        private void UpdateLevelCounter(int addedLevel)
+        {
+            itemLevelCounter[_listPosition] += addedLevel;
+            ValidateLevels();
+        }
+
+        private void ValidateLevels()
+        {
+            var validationCounter = 0;
+            foreach (var itemLevel in itemLevelCounter)
+            {
+                if (itemLevel == 0)
+                {
+
+                    validationCounter += 1;
+                }
+            }
+
+            if (validationCounter == itemLevelCounter.Count)
+            {
+                confirmButton.interactable = false;
+                _noChanges = true;
+            }
+            else
+            {
+                confirmButton.interactable = true;
+                _noChanges = false;
+            }
+        }
+
+        public void ResetItemLevelCounter()
+        {
+            for (var index = 0; index < itemLevelCounter.Count; index++)
+            {
+                itemLevelCounter[index] = 0;
+            }
+            
+            confirmButton.interactable = false;
+            _noChanges = true;
+        }
+        #endregion
         
         private void OnEnable()
         {
             UpgradeShopEvents.OnInsufficientFunds += TriggerNoMoneyUI;
+            UpgradeShopEvents.OnUpdateItemLevel += UpdateLevelCounter;
         }
 
         private void OnDisable()
         {
             UpgradeShopEvents.OnInsufficientFunds -= TriggerNoMoneyUI;
+            UpgradeShopEvents.OnUpdateItemLevel -= UpdateLevelCounter;
         }
     }
 }
