82ad76174d6910c547d50df99ce8f726b3b489e1
Ty <73094402+tynics@users.noreply.github.com>
2024-09-25 12:31:57 +0800

    Loading Screen

---
+++ Assets/Scripts/UI/LoadScreen.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;    
+
+public class LoadScreen : MonoBehaviour
+{
+    [Header("Loading Screen")]
+    [SerializeField] private Slider progressBar;
+    [SerializeField] private TMP_Text progressCount;  
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(LoadSceneAsync(2));
+    }
+    
+    IEnumerator LoadSceneAsync(int scene)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(scene);
+        
+        //Start loading bar
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress /.9f);
+
+            progressBar.value = progress;
+            progressCount.text = progress * 100 + "%";
+
+            yield return null;
+        }
+
+    }
+}
